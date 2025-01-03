ddf8736d1c3435fdb6adec735e128345cb83bc5f
Hsu, Yao-Chih <a102193c0717@gmail.com>
2025-01-03 15:36:21 +0800

    make helper.py

---
+++ helper.py
@@ -0,0 +1,7 @@
+"""
+Title: helper.py
+Editor: Hsu, Yao-Chih
+Version: 1140103
+Original version: autoFRK v1.4.3 (https://CRAN.R-project.org/package=autoFRK)
+Reference: Tzeng S, Huang H, Wang W, Nychka D, Gillespie C (2021). _autoFRK: Automatic Fixed Rank Kriging_. R package version 1.4.3, https://CRAN.R-project.org/package=autoFRK.
+"""

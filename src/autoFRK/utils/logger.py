274b9897bc6868593701e23099e8d873458b5444
Hsu, Yao-Chih <a102193c0717@gmail.com>
2025-11-09 14:05:47 +0800

    update for `torch.compile()` @torch._dynamo.disable

--- src/autoFRK/utils/logger.py
+++ src/autoFRK/utils/logger.py
@@ -7,10 +7,12 @@ Reference: References from the SSSDS4 model by Wen-Ting Wang from https://github
 """
 
 # import modules
+import torch
 import logging
 import colorlog
 
 # logger config
+@torch._dynamo.disable
 def setup_logger() -> logging.Logger:
     """
     Set up and return a logger with colored output.
@@ -53,6 +55,7 @@ def setup_logger() -> logging.Logger:
     return logger
 
 # set logger level
+@torch._dynamo.disable
 def set_logger_level(
     logger: logging.Logger,
     level: str | int
